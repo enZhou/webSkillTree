cda1e27e2c069f0b5159d1f0b992b74176e2088e
enZhou <758495585@qq.com>
2020-04-16 10:01:38 +0800

    feat:build

---
+++ precache-manifest.10b295b54c0bb54cc907ba0ab04ef652.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7148aabd4dc10bacc9fd2712c99773a6",
+    "url": "/skilltree/index.html"
+  },
+  {
+    "revision": "9ca2c4be5d566290e3c9",
+    "url": "/skilltree/static/css/0.9a1ccdc6.chunk.css"
+  },
+  {
+    "revision": "f766785d19471889e423",
+    "url": "/skilltree/static/css/1.54460208.chunk.css"
+  },
+  {
+    "revision": "7267daaef191bbb8621d",
+    "url": "/skilltree/static/css/4.b5826b3e.chunk.css"
+  },
+  {
+    "revision": "c86b1058feb12fc8596f",
+    "url": "/skilltree/static/css/main.4229a993.chunk.css"
+  },
+  {
+    "revision": "9ca2c4be5d566290e3c9",
+    "url": "/skilltree/static/js/0.8911883a.chunk.js"
+  },
+  {
+    "revision": "f766785d19471889e423",
+    "url": "/skilltree/static/js/1.13da5fcb.chunk.js"
+  },
+  {
+    "revision": "7267daaef191bbb8621d",
+    "url": "/skilltree/static/js/4.7fea863d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/skilltree/static/js/4.7fea863d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c86b1058feb12fc8596f",
+    "url": "/skilltree/static/js/main.96094f5c.chunk.js"
+  },
+  {
+    "revision": "4d96eef7e27fc6a46781",
+    "url": "/skilltree/static/js/runtime-main.5c91c8f0.js"
+  },
+  {
+    "revision": "c0fb020b7b440252fe7caf911a29b653",
+    "url": "/skilltree/static/media/adorn@2x.c0fb020b.png"
+  },
+  {
+    "revision": "e918866da9529977d53122f7b52e12ed",
+    "url": "/skilltree/static/media/aims-hover@2x.e918866d.png"
+  },
+  {
+    "revision": "f463289819e279a557361672dc2ac0c6",
+    "url": "/skilltree/static/media/appStore@2x.f4632898.png"
+  },
+  {
+    "revision": "87d547e4a34b776f065e99f3ca65cfb8",
+    "url": "/skilltree/static/media/bannerBg@2x.87d547e4.png"
+  },
+  {
+    "revision": "5004ddc7219d8ad05328ad82c670ec0c",
+    "url": "/skilltree/static/media/base@2x.5004ddc7.png"
+  },
+  {
+    "revision": "42f70fb875f7be6168bc84910ffe394f",
+    "url": "/skilltree/static/media/contact-line1@2x.42f70fb8.png"
+  },
+  {
+    "revision": "543571abbda060d560a48a7c15215866",
+    "url": "/skilltree/static/media/contact-line2@2x.543571ab.png"
+  },
+  {
+    "revision": "9f3ca1fba23094fbf2b408037961f879",
+    "url": "/skilltree/static/media/contact-us-logo@2x.9f3ca1fb.png"
+  },
+  {
+    "revision": "94d4700685cd1de864619f2066e56691",
+    "url": "/skilltree/static/media/contact-us@2x.94d47006.png"
+  },
+  {
+    "revision": "bccc4dda25a794445ae9d7933d0a659f",
+    "url": "/skilltree/static/media/contactUs-btn-bg@2x.bccc4dda.png"
+  },
+  {
+    "revision": "96f7ae1db408e68824bf550e0a9761b3",
+    "url": "/skilltree/static/media/explanation-bg-hover@2x.96f7ae1d.png"
+  },
+  {
+    "revision": "7bbf426146e7ad4e4eb8216d361e2637",
+    "url": "/skilltree/static/media/explanation-border@2x.7bbf4261.png"
+  },
+  {
+    "revision": "4f7e956003cd0b5fb022d647c0a7063b",
+    "url": "/skilltree/static/media/explanation-icon-border@2x.4f7e9560.png"
+  },
+  {
+    "revision": "80636bbb84875e60cb5c3fcc79cfe874",
+    "url": "/skilltree/static/media/feature-detail-bg@2x.80636bbb.png"
+  },
+  {
+    "revision": "dfa1e6a29aca52ea329f75e7d89dc3ff",
+    "url": "/skilltree/static/media/googlePlay@2x.dfa1e6a2.png"
+  },
+  {
+    "revision": "f707f331edf105a4c39915311a12c4fc",
+    "url": "/skilltree/static/media/lightning@2x.f707f331.png"
+  },
+  {
+    "revision": "0ed0495fdd404d16a1aa2f8e9cd06604",
+    "url": "/skilltree/static/media/line@2x.0ed0495f.png"
+  },
+  {
+    "revision": "ebe88d0b8519ddbc2439bec80113d972",
+    "url": "/skilltree/static/media/line@2x.ebe88d0b.png"
+  },
+  {
+    "revision": "19cc1a04c314162b7d08916ca83c96d6",
+    "url": "/skilltree/static/media/menu-hover@2x.19cc1a04.png"
+  },
+  {
+    "revision": "6207872c2d3814695704b6e6a7e104f8",
+    "url": "/skilltree/static/media/people@2x.6207872c.png"
+  },
+  {
+    "revision": "c1df863fac6b8da44ce271f75685016d",
+    "url": "/skilltree/static/media/wallet-hover@2x.c1df863f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c4cb1beb394d6daee24ea676ccd0c39b.js"
+  "/skilltree/precache-manifest.10b295b54c0bb54cc907ba0ab04ef652.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/skilltree/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
